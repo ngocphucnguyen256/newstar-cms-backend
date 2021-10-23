e529b49003467c2faf396783200145b9b982e1c1
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-10-23 20:26:33 +0700

    deletepkjon

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,6 +1,8 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
+//
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
