a67ffd4c83d7c16971d4e076dda039e13e1d6caf
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-10-29 09:23:12 +0700

    id

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -8,9 +8,9 @@ module.exports = {
    */
 
   async findOne(ctx) {
-    const { slug } = ctx.params;
+    const { id } = ctx.params;
 
-    const entity = await strapi.services.article.findOne({ slug });
+    const entity = await strapi.services.article.findOne({ id });
     return sanitizeEntity(entity, { model: strapi.models.article });
   },
 };
